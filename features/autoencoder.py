9fd5918d4655df6fc68d47b54e7da08f67ae23bb
sajadmanesh <sina.sajadmanesh@gmail.com>
2022-03-28 09:37:07 +0200

    train_encoder returns the encoder instead of predictions

--- features/autoencoder.py
+++ features/autoencoder.py
@@ -1,21 +1,10 @@
-import numpy as np
 from keras.layers import Input, LSTM, RepeatVector
 from keras.models import Model
-from sklearn.preprocessing import MinMaxScaler
 
 
-def encode(X_list, epochs=50, latent_factor=2):
-    #     start_time = time.time()
-    for i in range(1, len(X_list)):
-        X_list[i] -= X_list[i - 1]
-    scaler = MinMaxScaler(copy=False)
-    for X in X_list:
-        scaler.fit_transform(X)
-
-    X = np.stack(X_list, axis=1)  # X.shape = (n_samples, timesteps, n_features)
+def train_encoder(X, epochs=50, latent_factor=2):
     n_samples, timesteps, input_dim = X.shape
     latent_dim = int(latent_factor * input_dim)
-    #     x_train, x_test = train_test_split(X, stratify=Y)
 
     inputs = Input(shape=(timesteps, input_dim))
     encoded = LSTM(latent_dim)(inputs)
@@ -27,8 +16,7 @@ def encode(X_list, epochs=50, latent_factor=2):
     encoder = Model(inputs, encoded)
 
     sequence_autoencoder.compile(optimizer='adadelta', loss='mse')
-    history = sequence_autoencoder.fit(X, X[:, ::-1, :], epochs=epochs, batch_size=256, shuffle=True)
-    # sequence_autoencoder.save('autorencoder.model')
+    history = sequence_autoencoder.fit(X, X[:, ::-1, :], epochs=epochs, batch_size=1024, shuffle=True)
+    
     print('Autoencoder Training Loss: %.4f' % history.history['loss'][-1])
-    #     print(time.time()-start_time)
-    return encoder.predict(X)
+    return encoder
